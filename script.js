938b9bc5b4598a6669b90d742b55173e6490d11b
NIKHIL NAILWAL <78683932+nailwal08@users.noreply.github.com>
2023-11-13 13:42:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,209 @@
+function loco() {
+  gsap.registerPlugin(ScrollTrigger);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+  });
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+}
+loco()
+
+var clutter = "";
+document.querySelector("#page2>h1").textContent.split("").forEach(function (dets) {
+  clutter += `<span>${dets}</span>`
+  document.querySelector("#page2>h1").innerHTML = clutter;
+})
+
+gsap.to("#page2>h1>span", {
+  scrollTrigger: {
+    trigger: `#page2>h1>span`,
+    start: `top bottom`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub: .5,
+  },
+  stagger: .2,
+  color: `#1137ca`
+})
+
+var clutter = "";
+document.querySelector("#page4>h1").textContent.split("").forEach(function (dets) {
+  clutter += `<span>${dets}</span>`
+
+  document.querySelector("#page4>h1").innerHTML = clutter;
+})
+gsap.to("#page4>h1>span", {
+  scrollTrigger: {
+    trigger: `#page4>h1>span`,
+    start: `top bottom`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub: .5,
+  },
+  stagger: .2,
+  color: `#1137ca`
+})
+
+function canvas1() {
+  const canvas = document.querySelector("#page5>canvas");
+  const context = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+
+  window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    render();
+  });
+
+  function files(index) {
+    var data = `
+./img1.png
+./img2.png
+./img3.png
+./img4.png
+./img5.png
+./img6.png
+./img7.png
+./img8.png
+./img9.png
+./img10.png
+./img11.png
+./img12.png
+./img13.png
+./img14.png
+./img15.png
+./img16.png
+./img17.png
+./img18.png
+./img19.png
+./img20.png
+./img21.png
+./img22.png
+./img23.png
+./img24.png
+./img25.png
+./img26.png
+./img27.png
+./img28.png
+`;
+    return data.split("\n")[index];
+  }
+
+  const frameCount = 29;
+
+  const images = [];
+  const imageSeq = {
+    frame: 1,
+  };
+
+  for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = files(i);
+    images.push(img);
+  }
+
+  gsap.to(imageSeq, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: `none`,
+    scrollTrigger: {
+      scrub: .5,
+      trigger: `#page5`,
+      start: `top top`,
+      end: `250% top`,
+      scroller: `#main`,
+    },
+    onUpdate: render,
+  });
+
+  images[1].onload = render;
+
+  function render() {
+    scaleImage(images[imageSeq.frame], context);
+  }
+
+  function scaleImage(img, ctx) {
+    var canvas = ctx.canvas;
+    var hRatio = canvas.width / img.width;
+    var vRatio = canvas.height / img.height;
+    var ratio = Math.max(hRatio, vRatio);
+    var centerShift_x = (canvas.width - img.width * ratio) / 2;
+    var centerShift_y = (canvas.height - img.height * ratio) / 2;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(
+      img,
+      0,
+      0,
+      img.width,
+      img.height,
+      centerShift_x,
+      centerShift_y,
+      img.width * ratio,
+      img.height * ratio
+    );
+  }
+  ScrollTrigger.create({
+
+    trigger: "#page5",
+    pin: true,
+    scroller: `#main`,
+    start: `top top`,
+    end: `250% top`,
+  });
+}
+canvas1()
+
+var clutter = "";
+
+document.querySelector("#page6>h1").textContent.split("").forEach(function (dets) {
+  clutter += `<span>${dets}</span>`
+  document.querySelector("#page6>h1").innerHTML = clutter;
+})
+
+gsap.to("#page6>h1>span", {
+  scrollTrigger: {
+    trigger: `#page6>h1>span`,
+    start: `top bottom`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub: .5,
+  },
+  stagger: .2,
+  color: `#fff`
+})
+
+gsap.to(".page7-cir", {
+  scrollTrigger: {
+    trigger: `.page7-cir`,
+    start: `top center`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub: .5
+  },
+  scale: 1.5
+})
+
+gsap.to(".page7-cir-inner", {
+  scrollTrigger: {
+    trigger: `.page7-cir-inner`,
+    start: `top center`,
+    end: `bottom top`,
+    scroller: `#main`,
+    scrub: .5
+  },
+  backgroundColor: `#0a3bce91`,
+})
